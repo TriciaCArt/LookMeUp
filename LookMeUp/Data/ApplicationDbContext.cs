8c6a5ffa50cb3b2bf0eea5ea6c8d1415c1c1794f
TriciaCArt <illiannamama@gmail.com>
2022-04-27 10:39:28 -0400

    Add project files.

---
+++ LookMeUp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LookMeUp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LookMeUp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contact> Contacts { get; set; } = default!;
+        public virtual DbSet<Category> Categories { get; set; } = default!;
+
+    }
+}
---
+++ LookMeUp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LookMeUp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? AppUserID { get; set; }
+
+        [Required]
+        [Display(Name="Category Name")]
+        public string? Name { get; set; }
+
+        //ToDo: Add Virtuals
+        public virtual AppUser? AppUser { get; set; }
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+    }
+}
